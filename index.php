65ab88ad63fd52dc331bbe275850ee6564df3160
MyUsername <robertomazza99@gmail.com>
2022-08-22 13:41:55 +0200

    add style

--- index.php
+++ index.php
@@ -10,26 +10,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
 </head>
 <body>
     <h1>
        Primo esercizio PHP
     </h1>
-    <h3>
-        <?php
-            echo $paragraph;
-        ?>
-    </h3>
-    <h3>
-        La frase è lunga <?php echo strlen($paragraph); ?> caratteri
-    </h3>
-    <h3>
-        La frase senza la parola proibita è :
-        <?php
+    <div class="content">
+        <h2>
+            FRASE ORIGINALE :
+            <?php
+                echo $paragraph;
+            ?>
+        </h2>
+        <h3>
+            La frase è lunga <?php echo strlen($paragraph); ?> caratteri
+        </h3>
+        <h3>
+            La frase senza la parola proibita è :
+            <?php
             // Creiamo una nuova variabile per la nuova frase, in cui la parola presa dall'url con GET verrà sostituita con ***
-           $new_paragraph = str_replace($_GET['bad_word'],'***', $paragraph);
-           echo $new_paragraph;
-        ?> 
-    </h3>
+            $new_paragraph = str_replace($_GET['bad_word'],'***', $paragraph);
+            echo $new_paragraph;
+            ?> 
+        </h3>
+    </div>   
 </body>
 </html>
